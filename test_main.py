ec3e47b8f5381aaa9698a5d829aeec3e72dd1d1e
cmdlinebeep <joelwigton@gmail.com>
2020-05-17 08:16:20 -0600

    Add running tests as part of the build

--- test_main.py
+++ test_main.py
@@ -24,6 +24,7 @@ def client():
 
 def test_health(client):
     response = client.get('/')
+    assert False    # Intentionally break build to test pre_build pipeline
     assert response.status_code == 200
     assert response.json == 'Healthy'
 
